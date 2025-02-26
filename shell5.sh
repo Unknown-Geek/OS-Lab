7fc98c06ff9df7b83bb7bb8ab4168ccd0c786ea6
Shravan Nander Pandala <97742905+Unknown-Geek@users.noreply.github.com>
2025-02-26 14:07:17 +0000

    Chnages Added

--- shell5.sh
+++ shell5.sh
@@ -1,23 +1,26 @@
+#!/bin/bash
+
 read -p "Enter the lower bound  : " a
 read -p "Enter the upper bound  : " b
-echo "Prime numbers between $a and $b (including limits) are :"
-for	;
-do
-	if (($i==1)); then
-		continue
-	fi
-	
-	flag=0
-	limit=$(echo "$i/2" | bc)
-	for((j=2 ; j<=$limit ; j++));
-	do
-		mod=$(echo "$i%$j" | bc)
-		if (($mod==0)); then
-			flag=1
-			break
-		fi
-	done
-	if (($flag==0)); then
-		echo $i
-	fi
+
+echo "Prime numbers between $a and $b (including limits) are:"
+
+for ((i = a; i <= b; i++)); do
+    if ((i == 1)); then
+        continue  
+    fi
+
+    flag=0
+    limit=$((i / 2)) 
+
+    for ((j = 2; j <= limit; j++)); do
+        if ((i % j == 0)); then  
+            flag=1
+            break
+        fi
+    done
+
+    if ((flag == 0)); then
+        echo $i
+    fi
 done
